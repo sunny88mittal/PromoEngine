bffcab2b60612a01e5cb4f850da5d2bc953e995a
Sunny Mittal <sunmitta@microsoft.com>
2022-11-08 17:00:44 +0530

    Adding test cases for N Items promotion promotion

---
+++ PromoEngine/promoengine/src/test/java/com/sunny/promoengine/promotion/TestNItemsPromotion.java
@@ -0,0 +1,82 @@
+package com.sunny.promoengine.promotion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class TestNItemsPromotion {
+
+	private IPromotion nItemsPromotion;
+
+	@Before
+	public void setUp() {
+		nItemsPromotion = new NItemsPromotion("A", 3, 130);
+	}
+
+	@Test
+	public void TestCartWithNoPromoItems() {
+		List<String> cartItems = Arrays.asList("B", "C", "D", "B", "B");
+		List<String> cartItemsCopy = Collections.unmodifiableList(cartItems);
+		int price = nItemsPromotion.applyPromo(cartItems);
+		Assert.assertEquals(price, 0);
+		Assert.assertTrue(cartItems.equals(cartItemsCopy));
+	}
+
+	@Test
+	public void TestCartWithAllItemsEligibleForPromotion() {
+		List<String> cartItems = Arrays.asList("A", "C", "D", "A", "A");
+		List<String> cartItemsCopy = new ArrayList<String>();
+		cartItemsCopy.addAll(cartItems);
+
+		int price = nItemsPromotion.applyPromo(cartItemsCopy);
+		List<String> remainingCartItems = Arrays.asList("C", "D");
+
+		Assert.assertEquals(price, 130);
+		Assert.assertTrue(remainingCartItems.equals(cartItemsCopy));
+	}
+
+	@Test
+	public void TestCartWithSomesItemsEligibleForPromotion() {
+		List<String> cartItems = Arrays.asList("A", "A", "D", "A", "A");
+		List<String> cartItemsCopy = new ArrayList<String>();
+		cartItemsCopy.addAll(cartItems);
+
+		int price = nItemsPromotion.applyPromo(cartItemsCopy);
+		List<String> remainingCartItems = Arrays.asList("D", "A");
+
+		Assert.assertEquals(price, 130);
+		Assert.assertTrue(remainingCartItems.equals(cartItemsCopy));
+	}
+
+	@Test
+	public void TestCartWithMultipleItemSetsEligibleForPromotion() {
+		List<String> cartItems = Arrays.asList("A", "A", "D", "A", "A", "A", "A");
+		List<String> cartItemsCopy = new ArrayList<String>();
+		cartItemsCopy.addAll(cartItems);
+
+		int price = nItemsPromotion.applyPromo(cartItemsCopy);
+		List<String> remainingCartItems = Arrays.asList("D");
+
+		Assert.assertEquals(price, 260);
+		Assert.assertTrue(remainingCartItems.equals(cartItemsCopy));
+	}
+
+	@Test
+	public void TestCartWithItemsLessThanQuantityEligibleForPromotion() {
+		List<String> cartItems = Arrays.asList("A", "A", "D");
+		List<String> cartItemsCopy = new ArrayList<String>();
+		cartItemsCopy.addAll(cartItems);
+
+		int price = nItemsPromotion.applyPromo(cartItemsCopy);
+		List<String> remainingCartItems = Arrays.asList("A", "A", "D");
+
+		Assert.assertEquals(price, 0);
+		Assert.assertTrue(remainingCartItems.equals(cartItemsCopy));
+	}
+}
