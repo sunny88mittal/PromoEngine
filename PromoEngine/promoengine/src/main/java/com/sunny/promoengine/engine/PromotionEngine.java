6b708e28abef773fb42ba3054741f9f54722e0e2
Sunny Mittal <sunmitta@microsoft.com>
2022-11-08 17:12:25 +0530

    Minor fixes in promotion engine

--- PromoEngine/promoengine/src/main/java/com/sunny/promoengine/engine/PromotionEngine.java
+++ PromoEngine/promoengine/src/main/java/com/sunny/promoengine/engine/PromotionEngine.java
@@ -3,17 +3,21 @@ package com.sunny.promoengine.engine;
 import java.util.List;
 import java.util.Map;
 
-import com.sunny.promoengine.catalog.ProductPrices;
 import com.sunny.promoengine.promotion.IPromotion;
-import com.sunny.promoengine.promotion.PromotionsProvider;
 
 public class PromotionEngine {
 
-	private static List<IPromotion> promotions = PromotionsProvider.getPromotions();
+	private List<IPromotion> promotions;
 
-	private static Map<String, Integer> productPrices = ProductPrices.getProducPrices();
+	private Map<String, Integer> productPrices;
 
-	public static int applyPromotions(List<String> cartItems) {
+	public PromotionEngine(List<IPromotion> promotions, Map<String, Integer> productPrices) {
+		super();
+		this.promotions = promotions;
+		this.productPrices = productPrices;
+	}
+
+	public int applyPromotions(List<String> cartItems) {
 		int cartPrice = 0;
 		for (IPromotion promotion : promotions) {
 			cartPrice += promotion.applyPromo(cartItems);
