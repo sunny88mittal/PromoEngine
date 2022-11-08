903ca239cceaf1d0c86e0702aa80f1285dc7f177
Sunny Mittal <sunmitta@microsoft.com>
2022-11-08 16:27:40 +0530

    Adding code for the promotions engine

--- PromoEngine/promoengine/src/main/java/com/sunny/promoengine/engine/PromotionEngine.java
+++ PromoEngine/promoengine/src/main/java/com/sunny/promoengine/engine/PromotionEngine.java
@@ -1,6 +1,28 @@
 package com.sunny.promoengine.engine;
 
+import java.util.List;
+import java.util.Map;
+
+import com.sunny.promoengine.catalog.ProductPrices;
+import com.sunny.promoengine.promotion.IPromotion;
+import com.sunny.promoengine.promotion.PromotionsProvider;
+
 public class PromotionEngine {
 
-	
+	private static List<IPromotion> promotions = PromotionsProvider.getPromotions();
+
+	private static Map<String, Integer> productPrices = ProductPrices.getProducPrices();
+
+	public static int applyPromotions(List<String> cartItems) {
+		int cartPrice = 0;
+		for (IPromotion promotion : promotions) {
+			cartPrice += promotion.applyPromo(cartItems);
+		}
+
+		for (String cartItem : cartItems) {
+			cartPrice += productPrices.get(cartItem);
+		}
+
+		return cartPrice;
+	}
 }
